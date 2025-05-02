1962a6e934e40b12ee93f10b6ee79d501b276000
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-05-02 16:42:50 +0200

    [UPD] ARROW FUNCTION

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -5,14 +5,16 @@ buon pomeriggio se è pomeriggio (fino alle 17)
 e buonasera se è sera (oltre le 17)
 */
 
-const name = 'Mario';
+const fullName = 'Mario';
 
 
-// Dichiara la funzione qui.
+/* Dichiara la funzione qui.
 function dayTime(string){
+    
     // RECUPERO IL GIORNO ODIERNO ED IL MOMENTO ATTUALE PER ANDARE POI A RECUPERARE L'ORA ATTUALE
     const day = new Date();
     const hours = day.getHours();
+    
     // CONTROLLA IL VALORE DI hours
     if (hours <13){
         console.log (`Buongiorno ${string}`);
@@ -25,9 +27,29 @@ function dayTime(string){
     }
 
 }
+*/ 
+
+// ARROW FUNCTION DELLA FUNZIONE PRECEDENTE 
+const dayTime = (string) =>{
+    
+    // RECUPERO IL GIORNO ODIERNO ED IL MOMENTO ATTUALE PER ANDARE POI A RECUPERARE L'ORA ATTUALE
+    const day = new Date();
+    const hours = day.getHours();
+    
+    // CONTROLLA IL VALORE DI hours
+    if (hours <13){
+        console.log (`Buongiorno ${string}`);
+    }
+    else if (hours <7){
+        console.log (`Buon pomeriggio ${string}`);
+    }
+    else {
+        console.log (`Buona sera ${string}`);
+    }
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-dayTime(name);
+dayTime(fullName);
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
